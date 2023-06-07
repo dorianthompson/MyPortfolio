0a07c1e3a000112a2c565b1c6823f9757d201169
Dorian Thompson <thompsondorian2@gmail.com>
2023-06-07 14:44:36 -0400

    add initial css, png files, and header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import './header.css';
+
+const Header = () => {
+  const [Toggle, showMenu] = useState(false);
+
+  return (
+    <header className="header">
+        <nav className="nav container">
+            <a href="index.html" className="nav-logo">Dorian</a>
+
+            <div className={Toggle ? "nav-menu show-menu" : "nav-menu"}>
+                <ul className="nav-list grid">
+                    <li className="nav-item">
+                        <a href="#home" className="nav-link">
+                            <i className="uil uil-estate nav-icon"></i> Home
+                        </a>
+                    </li>
+
+                    <li className="nav-item">
+                        <a href="#about" className="nav-link">
+                            <i className="uil uil-user nav-icon"></i> About
+                        </a>
+                    </li>
+
+                    <li className="nav-item">
+                        <a href="#skills" className="nav-link">
+                            <i className="uil uil-file-alt nav-icon"></i> Skills
+                        </a>
+                    </li>
+
+                    <li className="nav-item">
+                        <a href="#portfolio" className="nav-link">
+                            <i className="uil uil-scenery nav-icon"></i> Portfolio
+                        </a>
+                    </li>
+
+                    <li className="nav-item">
+                        <a href="#contact" className="nav-link">
+                            <i class="uil uil-message nav-icon"></i> Contact
+                        </a>
+                    </li>
+                </ul>
+
+                <i className="uil uil-times nav-close" onClick={() => showMenu(!Toggle)}></i>
+            </div>
+            <div className="nav-toggle" style={{
+                    color: Toggle ? 'transparent' : ''
+                }} onClick={() => showMenu(!Toggle)}>
+                <i class="uil uil-apps"></i>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
