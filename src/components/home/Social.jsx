396fcf24678e06336a05fab5be88e40f0d96dee4
Dorian Thompson <thompsondorian2@gmail.com>
2023-06-07 19:15:29 -0400

    start home

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home-social">
+        <a href="https://www.linkedin.com/in/dorian-thompson-459171b4/" className="home-social-icon" target="_blank">
+            <i className="uil uil-github-alt"></i>
+        </a>
+
+        <a href="https://github.com/dorianthompson" className="home-social-icon" target="_blank">
+            <i className="uil uil-linkedin"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
