03e0ef7cf1056222697af0fe1a2184069b79ad44
Dorian Thompson <dorianthompson2@Dorians-MBP.fios-router.home>
2023-08-15 19:58:38 -0400

    update social links and add new Project to portfolio

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -2,52 +2,45 @@ import CrwnClothing from '../../assets/Projects/CrwnClothing.png';
 import NASAMissionControl from '../../assets/Projects/NASAMissionControl.png';
 import Natours from '../../assets/Projects/Natours.png';
 import PorfolioWebsite from '../../assets/Projects/PortfolioWebsite.png';
-import StopWatch from '../../assets/Projects/Stopwatch.png';
-import TicTacToe from '../../assets/Projects/TicTacToe.png';
+import Jobs from '../../assets/Projects/JobsAPI.jpeg'
 
 export const projectsData = [
     {
         id: 1,
+        image: Jobs,
+        title: 'Jobs API',
+        category: 'backend',
+        link: "https://jobs-pr26.onrender.com/api-docs/"
+    },
+    {
+        id: 2,
         image: PorfolioWebsite,
         title: 'Portfolio Website',
         category: 'frontend',
         link: "#"
     },
     {
-        id: 2,
+        id: 3,
         image: CrwnClothing,
         title: 'CrwnClothing',
         category: 'fullstack',
         link: "https://chipper-llama-057977.netlify.app/"
     },
     {
-        id: 3,
+        id: 4,
         image: NASAMissionControl,
         title: 'NASAMissionControl',
         category: 'backend',
         link: ""
     },
     {
-        id: 4,
+        id: 5,
         image: Natours,
         title: 'Natours Landing Page',
         category: 'frontend',
         link: "https://natours-sigma-ten.vercel.app/"
     },
-    {
-        id: 5,
-        image: TicTacToe,
-        title: 'TicTacToe',
-        category: 'frontend',
-        link: "https://tic-tac-f3gz4l2s1-dorianthompson.vercel.app/"
-    },
-    {
-        id: 6,
-        image: StopWatch,
-        title: 'Stopwatch',
-        category: 'frontend',
-        link: "https://stopwatch-delta-ecru.vercel.app/"
-    }
+    
 ];
 
 export const projectsNavigation = [
