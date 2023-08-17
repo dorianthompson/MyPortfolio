d735bf922f2328c3ba3ec250ab22267f824b073e
Dorian Thompson <dorianthompson2@Dorians-MBP.fios-router.home>
2023-08-17 19:46:10 -0400

    switch social icons

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -4,11 +4,11 @@ const Social = () => {
   return (
     <div className="home-social">
         <a href="https://www.linkedin.com/in/dorian-thompson-459171b4/" className="home-social-icon" target="_blank">
-            <i className="uil uil-github-alt"></i>
+            <i className="uil uil-linkedin-alt"></i>
         </a>
 
         <a href="https://github.com/dorianthompson" className="home-social-icon" target="_blank">
-            <i className="uil uil-linkedin"></i>
+            <i className="uil uil-github"></i>
         </a>
     </div>
   )
